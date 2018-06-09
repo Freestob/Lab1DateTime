57718c7be48c20f2e947760dc2a40f62a262c786
Freestob <bradley.freestone@gmail.com>
2018-06-09 12:52:29 -0500

    First commit for Lab 1 date time

---
+++ Lab1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            string dateOne, dateTwo;
+            Console.WriteLine("Write dates in yyyy/mm/dd format.");
+            Console.WriteLine("");
+            Console.WriteLine("Please enter date one");
+            dateOne = Console.ReadLine();
+
+            Console.WriteLine("Please enter date two");
+            dateTwo = Console.ReadLine();
+
+            Console.WriteLine(dateOne);
+            Console.WriteLine(dateTwo);
+        }
+    }
+}
