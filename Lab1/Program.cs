ed624c7f97e63eade7d7a858badef7cd0b6bb45e
Freestob <bradley.freestone@gmail.com>
2018-06-10 17:34:26 -0500

    Planning how to write code using DateTime and TimeSpan

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -4,15 +4,16 @@ namespace Lab1
 {
     class Program
     {
-        static void Main(string[] args)
-
+        static void Main()
         {
+            // String inputs from user for the dates
             string entryOne, entryTwo;
-            // This is the beginning where people enter in the dates to find the difference 
-            
+
+            // This is the beginning where people enter in the dates to find the difference
             Console.WriteLine("Write dates in yyyy/mm/dd format.");
             Console.WriteLine("");
             Console.WriteLine("Please enter date one.");
+
             //This converts what the user inputs into the DateTime
             entryOne = Console.ReadLine();
             DateTime dateOne = DateTime.Parse(entryOne);
@@ -24,6 +25,17 @@ namespace Lab1
 
             Console.WriteLine(dateOne);
             Console.WriteLine(dateTwo);
+
+            // What I have is starting date and the ending date. 
+            // I need the time span between the two dates
+
+
+            //-----------------------------------------
+
+            // 
+            // What I need is the hours, minutes, seconds between the two dates
+
+
         }
 
     }
